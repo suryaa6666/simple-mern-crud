16f36450793b9931c5e5b8e2ee2f1c2e4c96e7f1
suryaa6666 <suryaelidanto@gmail.com>
2021-10-28 09:11:25 +0700

    Realtime

--- src/App.js
+++ src/App.js
@@ -5,31 +5,54 @@ import "./App.css";
 function App() {
   const [foodName, setFoodName] = useState("");
   const [days, setDays] = useState(0);
-  
-  const [foodList, setFoodList] = useState([]);
   const [updateFoodName, setUpdateFoodName] = useState("");
-  
+  const [updateDays, setUpdateDays] = useState("");
+  const [foodList, setFoodList] = useState([]);
+
   useEffect(() => {
-    Axios.get("https://belajar-mern-crud.herokuapp.com/read").then((response) => {
-      setFoodList(response.data);
-    });
+    Axios.get("https://belajar-mern-crud.herokuapp.com/read").then(
+      (response) => {
+        setFoodList(response.data);
+      }
+    );
   }, []);
-  
+
   const addToList = () => {
     Axios.post("https://belajar-mern-crud.herokuapp.com/insert", {
       foodName: foodName,
       days: days,
+    }).then(() => {
+      setFoodList([...foodList, { foodName: foodName, daysSinceIAte: days }]);
     });
   };
-  
+
   const deleteItem = (id) => {
-    Axios.delete(`https://belajar-mern-crud.herokuapp.com/delete/${id}`);
+    Axios.delete(`https://belajar-mern-crud.herokuapp.com/delete/${id}`).then(
+      () => {
+        setFoodList(
+          foodList.filter((x) => {
+            return x._id !== id;
+          })
+        );
+      }
+    );
   };
-  
+
   const updateItem = (id) => {
-    Axios.put("https://belajar-mern-crud.herokuapp.com/update" , {
+    Axios.put("https://belajar-mern-crud.herokuapp.com/update", {
       id: id,
-      foodName: updateFoodName
+      foodName: updateFoodName,
+      daysSinceIAte: updateDays,
+    }).then(() => {
+      setFoodList(
+        foodList.filter((x) => {
+          if (x._id === id) {
+            x.foodName = updateFoodName;
+            x.daysSinceIAte = updateDays;
+          }
+          return x;
+        })
+      );
     });
   };
 
@@ -66,7 +89,16 @@ function App() {
                 setUpdateFoodName(event.target.value);
               }}
               placeholder="New food name..."
-            ></input>
+            ></input>{" "}
+            <br />
+            <input
+              type="text"
+              onChange={(event) => {
+                setUpdateDays(event.target.value);
+              }}
+              placeholder="New days..."
+            ></input>{" "}
+            <br />
             <button onClick={() => updateItem(val._id)}> Update </button> <br />
             <button onClick={() => deleteItem(val._id)}> Delete </button>
           </div>
